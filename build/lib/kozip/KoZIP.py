cc3fc6875916a6c465927b624bdcbf569cee81f7
Heekang Park <park.heekang33@gmail.com>
2022-01-05 16:16:20 +0000

    v1.1.1

--- build/lib/kozip/KoZIP.py
+++ build/lib/kozip/KoZIP.py
@@ -4,12 +4,14 @@ import re
 
 class KoZIP:
     def __init__(self):
-        old_zip_file = os.path.join(os.path.dirname(__file__), "old_zip.json")
-        new_zip_file = os.path.join(os.path.dirname(__file__), "new_zip.json")
+        self.old_zip_file = os.path.join(os.path.dirname(__file__), "old_zip.json")
+        self.new_zip_file = os.path.join(os.path.dirname(__file__), "new_zip.json")
 
-        self.old_zip = json.loads(''.join(self._readlines(old_zip_file)))
-        self.new_zip = json.loads(''.join(self._readlines(new_zip_file)))
-    
+        self.old_zip = None
+        self.new_zip = None
+
+        self.new_zipcode_masked = {}
+
     def _readlines(self, file):
         for enc in ["utf8", "utf-8-sig", "ansi", "cp949", "euc-kr"]:
             try:
@@ -62,20 +64,19 @@ class KoZIP:
         with open("new_zip.json", "w") as f:
             json.dump(data, f, indent=4, ensure_ascii=False)
     
-    def ZIPtoAddr(self, zipcode, depth=2, format="string"):
-        assert format in ["list", "string"], "Invalid param: format"
-        
-        zipcode = str(zipcode)
-        if re.fullmatch("\d{5}", zipcode):
-            data = self.new_zip
-        elif re.fullmatch("\d{3}-\d{3}", zipcode):
-            data = self.old_zip
-        elif re.fullmatch("\d{6}", zipcode):
-            zipcode = zipcode[0:3] + '-' + zipcode[3:6]
-            data = self.old_zip
+    def _getData(self, type="new"):
+        assert type in ["old", "new"], "Invalid param: type"
+
+        if type == "old":
+            if self.old_zip == None:
+                self.old_zip = json.loads(''.join(self._readlines(self.old_zip_file)))
+            return self.old_zip
         else:
-            raise Exception("Invalid param: zipcode")
-        
+            if self.new_zip == None:
+                self.new_zip = json.loads(''.join(self._readlines(self.new_zip_file)))
+            return self.new_zip
+
+    def _getAddr(self, data, zipcode, depth, format):
         if depth in [1, "1", "시도"]:
             result = []
             for loc1 in data[zipcode].keys():
@@ -137,8 +138,58 @@ class KoZIP:
                             
                         else: # format == "list"
                             result.append([loc1, loc2, loc3])
-        else:
-            raise Exception("Invalid param: depth")
         
         return result
+
+    def _mergeDict(self, dest_dict, src_dict):
+        if type(dest_dict) is list and type(src_dict) is list:
+            dest_dict = list(set(dest_dict + src_dict))
+            return
+        else:
+            for key in src_dict.keys():
+                if key not in dest_dict.keys():
+                    dest_dict[key] = src_dict[key]
+                else:
+                    self._mergeDict(dest_dict[key], src_dict[key])
+            return
+
+    def ZIPtoAddr(self, zipcode, depth=2, format="string"):
+        assert depth in [1, "1", "시도", 2, "2", "시군구", 3, "3", "도로명", 4, "4", "full"], "Invalid param: depth"
+        assert format in ["list", "string"], "Invalid param: format"
+
+        zipcode = str(zipcode)
+        if re.fullmatch("\d{5}", zipcode):
+            data = self._getData("new")
+        elif re.fullmatch("\d{3}-\d{3}", zipcode):
+            data = self._getData("old")
+        elif re.fullmatch("\d{6}", zipcode):
+            zipcode = zipcode[0:3] + '-' + zipcode[3:6]
+            data = self._getData("old")
+        else:
+            raise Exception("Invalid param: zipcode")
         
+        return self._getAddr(data, zipcode, depth, format)
+
+    def maskedZIPtoAddr(self, masked_zipcode, masking_letter="*", depth=1, format="string"):
+        assert type(masking_letter) is str and len(masking_letter) == 1, "Invalid param: masking_letter"
+        assert type(masked_zipcode) is str and len(masked_zipcode) == 5, "Invalid param: masked_zipcode"
+        assert len([x for x in masked_zipcode if not (x.isdigit() or x == masking_letter)]) == 0, "Invalid param: masked_zipcode"
+        assert depth in [1, "1", "시도", 2, "2", "시군구", 3, "3", "도로명", 4, "4", "full"], "Invalid param: depth"
+        assert format in ["list", "string"], "Invalid param: format"
+
+        full_data = self._getData("new")
+        masked_zipcode = masked_zipcode.replace(masking_letter, "*")
+
+        masked_type = "".join([("0" if c.isdigit() else "*") for c in masked_zipcode])
+        if masked_type not in self.new_zipcode_masked.keys():
+            self.new_zipcode_masked[masked_type] = dict()
+            target = self.new_zipcode_masked[masked_type]
+
+            for key, value in full_data.items():
+                masked_key = "".join([("*" if c == "*" else key[i]) for i, c in enumerate(masked_type)])
+                if masked_key not in target.keys():
+                    target[masked_key] = {}
+                
+                self._mergeDict(target[masked_key], value)
+
+        return self._getAddr(self.new_zipcode_masked[masked_type], masked_zipcode, depth, format)
