ec8da1181187776e06e0a04a4c37f7c73ea8e6e1
Heekang Park <park.heekang33@gmail.com>
2022-01-05 17:10:40 +0000

    v1.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="kozip",
-    version="1.1.2",
+    version="1.1.3",
     license="MIT",
     author="Heekang Park",
     author_email="park.heekang33@gmail.com",
